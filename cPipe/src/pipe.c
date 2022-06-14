45c81b1a90de5e394963a39651d573fc888d3b82
cloydsecuya <cloydvansecuya@gmail.com>
2022-06-14 20:33:28 +0800

    Source file for pipe.c
    This src file is for creating ordinary pipes using C

---
+++ cPipe/src/pipe.c
@@ -0,0 +1,26 @@
+/*
+	Document: pipe.c
+	Created on: June 14, 2022
+	Author: Cloyd Van S. Secuya
+	Description:
+		Using an ordinary pipe, we are to create parent-child processes that are
+		able to communicate and access each other.
+		Then, produce an output
+		coming from the input of the child which will be the output of the parent
+*/
+
+
+// HEADER FILES
+#include <stdio.h>
+#include <sys/types.h>
+#include <unistd.h>
+#include <sys/wait.h>
+
+
+
+// MAIN METHOD
+int main()
+{
+
+	return 0;
+}
