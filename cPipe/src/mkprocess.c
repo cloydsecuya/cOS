2d926976a60a3577ed7c604d27e9710c65929f5e
cloydsecuya <cloydvansecuya@gmail.com>
2022-06-14 20:28:09 +0800

    Making sub-processes

--- cPipe/src/mkprocess.c
+++ cPipe/src/mkprocess.c
@@ -13,7 +13,7 @@
 #include <stdio.h>
 #include <sys/types.h>
 #include <unistd.h>
-
+#include <sys/wait.h>
 
 
 
@@ -22,28 +22,47 @@ int main()
 {
 
 	// Processes
-	pid_t pid;
+	pid_t pid;		// first child of parent process
+	pid_t cpid;		// this will be the grand child
 
 	// Create process using fork()
 	pid = fork();
 
 
-	// Child process
+	// Child process (C1)
 	if (pid == 0)
 	{
 		// Get the created child process along with the parent
 		printf("I am a child, ID: %d\n", getpid());
 		printf("My parent is here, ID: %d\n", getppid());
 
+		// Create a grand child
+		cpid = fork();
+
+		// Child process 2 (C2) (created by this child)
+		if (cpid == 0)
+		{
+			printf("I am a grand child, ID: %d\n", getpid());
+			printf("My parent is, ID: %d\n", getppid());
+		}
+
+		// Parent process 2 (P2)
+		else
+		{
+			printf("I am a parent too, ID: %d\n", getpid());
+			printf("This is my child, ID: %d\n", cpid);
+			wait(NULL);
+		}
+
 	}
 
-	// Parent process
+	// Parent process (P1)
 	else
 	{
-		// If there are other values greater than 0 return
+		// If this is the parent 
 		printf("This is my child: %d\n", pid);
 		printf("I am a parent: %d\n", getpid());
-		sleep(8);
+		wait(NULL);
 	}
 
 	return 0;
