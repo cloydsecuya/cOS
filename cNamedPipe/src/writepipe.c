666e351ee42e1d96d8736d94956ab377f9b65aa6
Cloyd Van <cloydvansecuya@gmail.com>
2022-06-22 07:33:41 +0800

    Updated the writing pipe

--- cNamedPipe/src/writepipe.c
+++ cNamedPipe/src/writepipe.c
@@ -3,7 +3,9 @@
 	Created on: June 22, 2022
 	Author: Cloyd Van S. Secuya
 	Description:
-		Creating and using named pipes to communicate between processes
+		Creating and using named pipes to communicate between processes. 
+		Moreover, we try to use this file as the write only file which 
+		should write onto a generated file by mknod()
 */
 
 
@@ -21,13 +23,14 @@
 
 
 // MAIN METHOD
-int main()
+int main(int argc, char* argv[])
 {
-	// Process
-	pid_t pid;
 
 	// File Descriptor
 	int fd;
+
+	// Variable for writing 
+	int n; 
 
 	// File name
 	char filename[] = "file";
@@ -36,62 +39,34 @@ int main()
 	char buffer[BUFFSIZE];
 
 	// Start creating a named pipe and apply error checking
-	if (mknod(filename, 0666, 0) == -1)
+	fd = mknod(filename, 0666, 0);
+	if (fd == -1)
 	{
 		printf("An error occurred at creating the file \n");
 		return 1;
 	}
 
-
-	// Start forking
-	pid = fork();
-
-
-	// Check if there are any errors if returned -1
-	if(pid == -1)
+	// Start opening
+	prinf("Opening file... >>> %s\n", filename); 
+	fd = open(filename, O_WRONLY);
+	// Check for any errors in opening the  file
+	if (fd == -1) 
 	{
-		printf("Error occurred on spawning a process \n");
+		printf("An error occurred an opening the file >> %s\n", filename);
 		return 2;
 	}
 
-
-	// CHILD PROCESS	where we are supposed to write
-	if (pid == 0)
-	{
-		// Check to see if there are any errors for opening the named pipe
-		printf("Checking... \n");
-		if (fd = open("write.fifo", O_WRONLY) < 0)
+	// Try and write into the file while this program is running
+	while((n = read(STDIN_FILENO, buffer, BUFFSIZE)) > 0) 
+	{	// Try and begin writing. Apply error handling and checking
+		if (write(fd, buffer, strlen(buffer)) == -1) 
 		{
-			printf("An error occurred at opening the named pipe \n");
+			printf("An error occurred writing into the file!"); 
 			return 3;
 		}
-
-		else
-		{
-			printf("Success in creating a named pipe \n");
-		}
-
-		printf("Opening... \n");
-		int x = 97;
-		if( write(fd, &x, sizeof(x)) == -1)
-		{
-			printf("Error occurred at opening \n");
-			return 4;
-		}
-		printf("Wrote into file \n");
-		close(fd);
-		printf("Closed \n");
-
+		printf("Wrote >> %s ( %d ) bytes \n", buffer, strlen(buffer));
 	}
-
-
-
-
-
-
-
-
-
-
-
+	// Close the fd
+	close(fd);
+	return 0;
 }
