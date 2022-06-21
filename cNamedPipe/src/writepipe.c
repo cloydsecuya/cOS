912b25062fc94df0fb6d93507f530a7f684ba043
cloydsecuya <cloydvansecuya@gmail.com>
2022-06-22 03:36:21 +0800

    Renamed and staged writepipe.c

---
+++ cNamedPipe/src/writepipe.c
@@ -0,0 +1,26 @@
+/*
+	Document: writepipe.c
+	Created on: June 22, 2022
+	Author: Cloyd Van S. Secuya
+	Description:
+		Creating and using named pipes to communicate between processes
+*/
+
+
+
+// HEADER FILES
+#include <stdio.h>
+#include <sys/types.h>
+#include <unistd.h>
+#include <sys/wait.h>
+#include <string.h>
+
+
+
+// MAIN METHOD
+int main()
+{
+	// Process
+	pid_t pid;
+
+}
