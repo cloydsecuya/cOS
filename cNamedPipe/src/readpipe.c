324491c92e10e63580f4a446612729431c1d2645
cloydsecuya <cloydvansecuya@gmail.com>
2022-06-22 11:21:41 +0800

    Update to out files and src files

--- cNamedPipe/src/readpipe.c
+++ cNamedPipe/src/readpipe.c
@@ -3,8 +3,8 @@
 	Created on: June 22, 2022
 	Author: Cloyd Van S. Secuya
 	Description:
-		The other end of the named pipe where we try to 
-        open the file with read permissions
+		The other end of the named pipe where we try to
+        	open the file with read permissions
 */
 
 
@@ -26,57 +26,66 @@
 // MAIN METHOD
 int main(int argc, char* argv[])
 {
-    // File Descriptor
+    	// File Descriptor
 	int fd;
 
-    // Buffer 
-    char buffer[BUFFSIZE];
+    	// Buffer
+    	char buffer[BUFFSIZE];
 
-    // Byte size entered from stdin
-    int byte_size; 
+    	// Byte size entered from stdin
+    	int byte_size;
 
-	// Bytes as message
-	int bytes;
+    	// Bytes as message
+    	int bytes;
 
-	// Line end to determine until next input is finished
-	char line_end[10]; 
-	int nextLine;
+    	// Line end to determine until next input is finished
+    	char line_end[10];
+    	int nextLine;
 
 	// File name
 	char filename[] = "file";
 
+	printf("Making name pipe -  %s\n", filename);
+	printf("***CREATING FILE**** \n");
 	// Start a named pipe
 	mknod(filename, S_IFIFO|0640, 0);
 	strcpy(line_end, "end");
-    // Try and read into the file while this program is running
-	while(1) 
-	{	
-		// Start opening the file with read permission
-		printf("Opening file... >>> %s\n", filename); 
+
+		// Start reading the file with read permission
+		printf("Opening file... >>> %s\n", filename);
 		fd = open(filename, O_RDWR);
-		if (fd == -1) 
+		if (fd == -1)
 		{
-			printf("An error occurred an opening the file >> file");
+			printf("An error occurred an opening the file >> file \n");
 			return 1;
 		}
 
+		else
+		{
+			printf("File Opened! \n");
+		}
+
+	// Try and read into the file while this program is running
+	while(1)
+	{
 		// Read the stdin in buffer
 		/**
-		 * @NOTE: REMEMBER THAT WE ARE USING CHAR ARRAYS AS THERE 
-		 * 		  ARE NO STRING TYPE TO C. 
+		 * @NOTE: REMEMBER THAT WE ARE USING CHAR ARRAYS AS THERE
+		 * 		  ARE NO STRING TYPE TO C.
 		 * 		  SO \0 IS NEEDED!!!!!!
-		 * 
+		 *
 		 */
 		bytes =  read(fd, buffer, sizeof(buffer));
-		buffer[bytes] = '\0'; 				// this is needed for printing later on
-		byte_size = (int)strlen(buffer);	// cast to int
-		printf("\nRead >>> %s with %d in size \n", buffer, byte_size);
+		//buffer[bytes] = '\0'; 				// this is needed for printing later on
+		byte_size = (int)strlen(buffer);		// cast to int
+		printf("\nRead: %s   %d bytes in size \n", buffer, byte_size);
 
 		nextLine = strcmp(buffer, line_end);
-      	if (nextLine == 0) {
+      		if (nextLine == 0)
+		{
 			close(fd);
 			break;
-      	}
+      		}
 	}
 
     return 0;
--- cNamedPipe/src/writepipe.c
+++ cNamedPipe/src/writepipe.c
@@ -41,23 +41,23 @@ int main(int argc, char* argv[])
 	char buffer[BUFFSIZE];
 
 	// Start opening
-	printf("Opening file... >>> %s\n", filename); 
+	printf("Opening file... >>> %s\n", filename);
 	fd = open(filename, O_WRONLY);
 	// Check for any errors in opening the  file
-	if (fd == -1) 
+	if (fd == -1)
 	{
 		printf("An error occurred an opening the file >> %s\n", filename);
 		return 2;
 	}
-	
+
 	// Try and write into the file while this program is running
-	while((n = read(STDIN_FILENO, buffer, BUFFSIZE)) > 0) 
-	{	printf("\nPass message: "); 
+	while((n = read(STDIN_FILENO, buffer, BUFFSIZE)) > 0)
+	{	printf("\nPass message: ");
 		fgets(buffer, sizeof(buffer), stdin);
 		// Try and begin writing. Apply error handling and checking
-		if (write(fd, buffer, strlen(buffer)) == -1) 
+		if (write(fd, buffer, strlen(buffer)) == -1)
 		{
-			printf("An error occurred writing into the file! \n"); 
+			printf("An error occurred writing into the file! \n");
 			return 3;
 		}
 		//printf("Wrote >> %s ( %d ) bytes \n", buffer, strlen(buffer));
